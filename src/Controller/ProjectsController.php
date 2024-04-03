9cb40339011ac56ccadf4fcc280dff0f3b7f5ae0
Анохин Дмитрий <and@jcat.ru>
2024-04-03 10:07:48 +0300

    redirects

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -15,4 +15,22 @@ class ProjectsController extends AbstractController
             'controller_name' => 'ProjectsController',
         ]);
     }
+
+    /**
+     * @Route("/project/{id}", name="project_details")
+     */
+    public function details($id): Response
+    {
+        // Вместо реальных данных о проекте здесь могут быть запросы к базе данных
+        $project = [
+            'id' => 1,
+            'name' => 'Название проекта ' . 1,
+            'description' => 'Описание проекта ' . 1,
+        ];
+
+        // Заглушка для метода details контроллера проектов
+        return $this->render('project/details.html.twig', [
+            'project' => $project,
+        ]);
+    }
 }
